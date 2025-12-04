e76bb0fdc7bd06fd1a0ac6aefe429c09422e46fd
T-Rex <trungminhle2004@gmail.com>
2025-12-04 11:31:33 +0700

    init Lab3

---
+++ Lab3/lab3/lab3/Data/lab3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using lab3.Models;
+
+namespace lab3.Data
+{
+    public class lab3Context : DbContext
+    {
+        public lab3Context (DbContextOptions<lab3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab3.Models.Movie> Movie { get; set; } = default!;
+    }
+}
